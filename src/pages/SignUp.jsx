170968a4f2bd3f83eca58de2e06fe0d1ddb22d66
Arniel Candilansa <arniel.candilansa@gmail.com>
2026-03-23 09:25:45 +0800

    Added signup inputs

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,35 @@
+import Input from "../components/form/Input";
 import MainLayout from "../layouts/MainLayout";
 
 const SignUp = () => {
-	return <MainLayout>This is the Signup</MainLayout>;
+	return (
+		<MainLayout>
+			<Input
+				name="firstname"
+				placeholder="Enter your First Name"
+				label="Firstname"
+				type="text"
+			/>
+			<Input
+				name="lastname"
+				placeholder="Enter your Last Name"
+				label="Lastname"
+				type="text"
+			/>
+			<Input
+				name="email"
+				placeholder="Enter your Email"
+				label="Email"
+				type="email"
+			/>
+			<Input
+				name="password"
+				placeholder="Enter your Password"
+				label="Password"
+				type="password"
+			/>
+		</MainLayout>
+	);
 };
 
 export default SignUp;
